b7166339ecab8367fef360e6f9705fab529ee2ab
Thiago Fassina <thiagoaaa@gmail.com>
2024-04-06 01:21:21 -0300

    add auth/me endpoint so that we can test jwt guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Request, Post, UseGuards } from "@nestjs/common";
+import { Controller, Request, Post, UseGuards, Get } from "@nestjs/common";
 import { LocalAuthGuard } from "./guards/local-auth.guard";
 import { AuthService } from "./auth.service";
 import { ISessionUser } from "./interfaces/session-user.interface";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 
 @Controller("auth")
 export class AuthController {
@@ -11,5 +12,11 @@ export class AuthController {
   @Post("login")
   async login(@Request() req: { user: ISessionUser }) {
     return this.authService.login(req.user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get("me")
+  async me(@Request() req: { user: ISessionUser }) {
+    return req.user;
   }
 }
