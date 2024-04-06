de911d9caf4cb6abe8aae9ade9a2740bc3674f7a
Thiago Fassina <thiagoaaa@gmail.com>
2024-04-06 00:13:40 -0300

    finding cats from database

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from "@nestjs/common";
 import { ICat } from "./interfaces/cat.interface";
+import { Repository } from "typeorm";
+import { Cat } from "./cat.entity";
+import { InjectRepository } from "@nestjs/typeorm";
 
 @Injectable()
 export class CatsService {
   private readonly cats: ICat[] = [];
 
+  constructor(
+    @InjectRepository(Cat)
+    private catsRepository: Repository<Cat>,
+  ) {}
+
   create(cat: ICat) {
     this.cats.push(cat);
   }
 
-  findAll(): ICat[] {
-    return this.cats;
+  findAll(): Promise<Cat[]> {
+    return this.catsRepository.find();
   }
 }
